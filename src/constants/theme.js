2da36a21b850156da1fe0437b12ee55180f90b84
Jacob Toye <jacob.toye@gmail.com>
2017-09-29 09:38:03 +1300

    Add in theme constants.

---
+++ src/constants/theme.js
@@ -0,0 +1,18 @@
+export const colors = {
+  BACKGROUND: '#0c0b0c',
+
+  PRIMARY: '#edcac3',
+  ACCENT: '#d3a165',
+
+  BLACK_TEXT_PRIMARY: '#0c0b0c',
+
+  WHITE_TEXT_PRIMARY: 'rgba(255, 255, 255, 1)',
+};
+
+export const TextSizes = {
+  DISPLAY: 24,
+  TITLE: 20,
+  SUB_TITLE: 16,
+  BODY: 14,
+  DETAIL: 12,
+};
