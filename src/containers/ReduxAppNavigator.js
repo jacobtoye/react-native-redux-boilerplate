ec654a1aa3045a2d1e96e2386286ec75f5a62be8
Jacob Toye <jacob.toye@gmail.com>
2017-10-01 22:42:58 +1300

    Add support for the back button to the AppNavigator.

--- src/containers/ReduxAppNavigator.js
+++ src/containers/ReduxAppNavigator.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { addNavigationHelpers, StackNavigator } from 'react-navigation';
-
+import { BackHandler } from 'react-native';
 import {
-  SignInScreen,
-  WelcomeScreen,
-} from 'screens';
+  addNavigationHelpers,
+  NavigationActions,
+  StackNavigator,
+} from 'react-navigation';
+
+import { WelcomeScreen } from 'screens';
+import SignInScreen from './SignInScreen';
 
 export const AppNavigator = StackNavigator({
   SignIn: { screen: SignInScreen },
@@ -17,10 +20,36 @@ export const AppNavigator = StackNavigator({
   },
 });
 
-// TODO: Add in support for the Android back button
-const Navigation = ({ dispatch, nav }) => (
-  <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
-);
+class Navigation extends React.Component {
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  onBackPress = () => {
+    const { dispatch, nav } = this.props;
+
+    if (nav.index === 0) {
+      return false;
+    }
+
+    dispatch(NavigationActions.back());
+    return true;
+  }
+
+  render() {
+    const { dispatch, nav } = this.props;
+
+    return (
+      <AppNavigator
+        navigation={addNavigationHelpers({ dispatch, state: nav })}
+      />
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   nav: state.nav,
